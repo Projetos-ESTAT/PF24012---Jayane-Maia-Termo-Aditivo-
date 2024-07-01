4a23b3e3daff13dab3fb22dda206392fa82bd4f4
Gabriela Ludovico <gabi10ludovico@gmail.com>
2024-07-01 15:02:57 -0300

    Alteracoes.

--- rdocs/Gabi.R
+++ rdocs/Gabi.R
@@ -32,17 +32,17 @@ banco_deputados1 <- Base_deputados_provinciais_AR_1983_2023_1_ %>% group_by(yr,c
             Gi = sum(ifelse(gov_oppos=="G",pv1,0)),
             Gi2 = sum((ifelse(gov_oppos=="G",pv1,0))^2),
             G=Gi2/Gi,
-            IC = 1- abs(O-G)/100) 
-banco_deputados1
+            "ECI dep" = 1- abs(O-G)/100,
+            ENP = 1/(sum((pv1/100)^2)),) 
+banco_deputados1 <- banco_deputados1 %>%
+  rename(Year = yr, Province = cst_n)
 
 banco_deputados1 <- na.omit(banco_deputados1)
-banco_deputados1 <- banco_deputados1 %>%
-  select(yr, cst_n,IC)
 write.csv2(banco_deputados1)  
 
 # Calculo dos indices banco governadores
 
-Base_modificada1<- Base_modificada1 %>%
+Base_modificada1<- Base_governadores_certa %>%
   filter(!is.na(first_second))
 
 
@@ -56,12 +56,72 @@ banco_governadores1 <- Base_modificada1 %>% group_by(Year,Province) %>%
             "ECI gov/opos" = 1- abs(O-G)/100,
             p1 = sum(ifelse(first_second=="first",`Votes party (percent)`/100,0)), #índice 2
             p2 = sum(ifelse(first_second=="second",`Votes party (percent)`/100,0)), #índice 2
-            "ECI 1st/2nd" = ifelse(1-(p1-p2)>1,1,1-(p1-p2)) #índice 2
+            "ECI 1st/2nd" = ifelse(1-(p1-p2)>1,1,1-(p1-p2)), #índice 2
+            ENP = 1/(sum((`Votes party (percent)`/100)^2)),
   )
 
-banco_governadores1 <- banco_governadores1 %>% select(Year,Province,"ECI gov/opos","ECI 1st/2nd")
-banco_governadores1
-
 write.csv2(banco_governadores1) 
+
+
+# Teste de correlação entre ECI dep e ECI gov (1st/2nd)
+
+banco_deputados1 <- Base_deputados_provinciais_AR_1983_2023_1_ %>% group_by(yr,cst_n) %>%
+  summarise(Oi = sum(ifelse(gov_oppos=="O",pv1,0)),
+            Oi2 = sum((ifelse(gov_oppos=="O",pv1,0))^2),
+            O=Oi2/Oi,
+            Gi = sum(ifelse(gov_oppos=="G",pv1,0)),
+            Gi2 = sum((ifelse(gov_oppos=="G",pv1,0))^2),
+            G=Gi2/Gi,
+            "ECI dep" = 1- abs(O-G)/100,
+            ENP = 1/(sum((pv1/100)^2)),) 
+banco_deputados1 <- banco_deputados1 %>%
+  rename(Year = yr, Province = cst_n)
+
+banco_deputados1 <- na.omit(banco_deputados1)
+
+banco_governadores1 <- Base_modificada1 %>% group_by(Year,Province) %>%
+  summarise(Oi = sum(ifelse(gov_oppos=="O",`Votes party (percent)`,0)),
+            Oi2 = sum((ifelse(gov_oppos=="O",`Votes party (percent)`,0))^2),
+            O = Oi2/Oi,
+            Gi = sum(ifelse(gov_oppos=="G",`Votes party (percent)`,0)),
+            Gi2 = sum((ifelse(gov_oppos=="G",`Votes party (percent)`,0))^2),
+            G = ifelse(Gi==0,0,Gi2/Gi),
+            "ECI gov/opos" = 1- abs(O-G)/100,
+            p1 = sum(ifelse(first_second=="first",`Votes party (percent)`/100,0)), #índice 2
+            p2 = sum(ifelse(first_second=="second",`Votes party (percent)`/100,0)), #índice 2
+            "ECI 1st/2nd" = ifelse(1-(p1-p2)>1,1,1-(p1-p2)), #índice 2
+            ENP = 1/(sum((`Votes party (percent)`/100)^2)),
+  )
+
+banco_governadores1 <- banco_governadores1 %>% select(Year,Province,"ECI gov/opos","ECI 1st/2nd", "ENP")
+
+banco_deputados21 <- left_join(banco_deputados1,banco_governadores1[,c(1,2,4)],by = c('Year','Province'))
+
+banco_deputados21 <- banco_deputados21 %>% select(Year,Province,"ECI dep","ECI 1st/2nd", ENP)
+
+banco_deputados21 <- rename(banco_deputados21, "ECI gov" = "ECI 1st/2nd")
+
+banco_deputados21 <- na.omit(banco_deputados21)
+
+
+banco_deputadosCorrelacao <- banco_deputados21 %>% group_by(Province) %>%
+  do(tidy(cor.test(.$`ECI dep`, .$ `ECI gov`)))
+
+write.csv2(banco_deputadosCorrelacao)
+
+#Teste de correlação entre os dois índices de competitividade calculados para o banco governadores, isto é, ECI gov/opos e ECI first/second. 
+
+banco_governadoresCorrelacao <- banco_governadores1 %>%
+  group_by(Province) %>%
+  do(tidy(cor.test(.$`ECI gov/opos`, .$ `ECI 1st/2nd`)))
+
+
+write.csv2(banco_governadoresCorrelacao)
+
+
+
+
+
+
 
 
